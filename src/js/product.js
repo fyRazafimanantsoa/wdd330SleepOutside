cb083332e1d006d8a2d5faaf1ae68d0f67d9796c
fyRazafimanantsoa <fyratsoa@gmail.com>
2025-09-20 22:48:31 +0300

    Week 2 team activities

--- src/js/product.js
+++ src/js/product.js
@@ -1,18 +1,19 @@
-import { setLocalStorage } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import { getParam } from '../js/utils.mjs';
+import ProductData from '../js/ProductData.mjs';
+import ProductDetails from '../js/ProductDetails.mjs';
 
-const dataSource = new ProductData("tents");
+const dataSource = new ProductData('tents');
+const productID = getParam('product');
 
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+const product = new ProductDetails(productID, dataSource);
+product.init();
+
+/*async function addToCartHandler(e) {
+    const product = await dataSource.findProductById(e.target.dataset.id);
+    addProductToCart(product);
 }
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
 // add listener to Add to Cart button
 document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+  .getElementById('addToCart')
+  .addEventListener('click', addToCartHandler);
+*/
