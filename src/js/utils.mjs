6f1ad642fe3b2f56e7441c1ab6be6d171c5b8b1e
fyRazafimanantsoa <fyratsoa@gmail.com>
2025-09-20 23:22:07 +0300

    Update in vite.config.js/ main.js/utils.mjs for week 2 individual

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -2,18 +2,15 @@
 export function qs(selector, parent = document) {
   return parent.querySelector(selector);
 }
-// or a more concise version if you are into that sort of thing:
-// export const qs = (selector, parent = document) => parent.querySelector(selector);
 
-// retrieve data from localstorage
 export function getLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
 }
-// save data to local storage
+
 export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
-// set a listener for both touchend and click
+
 export function setClick(selector, callback) {
   qs(selector).addEventListener('touchend', (event) => {
     event.preventDefault();
@@ -32,7 +29,7 @@ export function getParam(param) {
 
 export function renderListWithTemplate(template, parentElement, list, position = 'afterbegin', clear = false) {
   const htmlStrings = list.map(template);
-  // if clear is true we need to clear out the contents of the parent.
+
   if (clear) {
     parentElement.innerHTML = '';
   }
