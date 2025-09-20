1a98e28326b6b491169d956413e284c9ede0c79a
fyRazafimanantsoa <fyratsoa@gmail.com>
2025-09-20 23:22:07 +0300

    Update in vite.config.js/ main.js/utils.mjs for week 2 individual

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -29,7 +29,6 @@ export default class ProductList {
     const htmlStrings = list.map(productCardTemplate);
     this.listElement.insertAdjacentHTML('afterbegin', htmlStrings.join(''));
 
-    // apply use new utility function instead of the commented code above
     renderListWithTemplate(productCardTemplate, this.listElement, list, 'afterbegin');
 
   }
