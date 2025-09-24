0727b731be1f67fb100899928c58ed1be813a4d7
Henrijo123 <henrijo123@gmail.com>
2025-09-24 09:58:07 -0300

    Team activity finished

--- src/js/product.js
+++ src/js/product.js
@@ -1,16 +1,18 @@
 
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import getParam from "./utils.mjs";
+import ProductDetails from "./ProductDetails.mjs";
+
+const product = new ProductDetails(productId, dataSource);
+product.init();
 
 const dataSource = new ProductData("tents");
-const productId = getParam('product');
+const productId = getParam("product");
 
 console.log(dataSource.findProductById(productId));
 
-function addProductToCart(product) {
-  const items = getLocalStorage("so-cart") || [];
-  items.push(product);
-  setLocalStorage("so-cart", items);
+export function addProductToCart(product) {
+  
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
